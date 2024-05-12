6f0173c2360ca81e884f6f04f766001808ae1540
Firstp1ck <kortharshadowbreath@gmail.com>
2024-05-12 15:16:10 +0200

    updated config

--- config.py
+++ config.py
@@ -5,6 +5,8 @@ config.read('config.ini')
 
 data_dir = config['paths']['data_dir']
 log_dir = config['paths']['log_dir']
+db_path = config['database']['db_path']
 
 print("Data Directory:", data_dir)
 print("Log Directory:", log_dir)
+print("Database Directory:", db_path)
