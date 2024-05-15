74a45bcca340d683fd6d6d9829eebb0f2dac2ed9
Firstp1ck <kortharshadowbreath@gmail.com>
2024-05-15 21:43:15 +0200

    added more structure

--- setup.py
+++ setup.py
@@ -1,18 +1,24 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Template-Repo',
+    name='Template_Repo',
     version='0.1.0',
     author='Firstpick',
     author_email='kortharshadowbreath@gmail.com',
-    description='Template for all my Repos',
+    description='Template for all my Repositorys',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='http://github.com/firstpick/Template-Repo',
-    packages=find_packages(where='src'),  # where your Python packages are
-    package_dir={'': 'src'},
+    url='http://github.com/firstpick/Template-Repo', # private
+    packages=find_packages(),
     install_requires=[
-        'Example_Lib',  # List your project's dependencies here. They will be installed automatically when using pip to install your package
+        'altgraph==0.17.4',
+        'packaging==24.0',
+        'pefile==2023.2.7',
+        'pyinstaller==6.6.0',
+        'pyinstaller-hooks-contrib==2024.6',
+        'python-dotenv==1.0.1',
+        'pywin32-ctypes==0.2.2',
+        'setuptools==69.5.1'
     ],
     classifiers=[
         'Development Status :: 3 - Alpha',
