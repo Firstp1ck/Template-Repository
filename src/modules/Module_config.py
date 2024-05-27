8b8aed681d82b0a92033b86c410a7b1130f0e371
Firstp1ck <kortharshadowbreath@gmail.com>
2024-05-27 22:20:42 +0200

    config update

--- src/modules/Module_config.py
+++ src/modules/Module_config.py
@@ -16,39 +16,40 @@ def validate_file_path(path):
     else:
         logging.info(f"File validated and exists: {path}")
 
-# Read and process the configuration
-try:
-    if not os.path.exists(config_file_path):
-        logging.error(f"Configuration file not found at {config_file_path}")
-        raise FileNotFoundError(f"Configuration file not found at {config_file_path}")
-    
-    config.read(config_file_path)
-    logging.info("Configuration file has been read successfully")
+if __name__ == "__main__":
+    # Read and process the configuration
+    try:
+        if not os.path.exists(config_file_path):
+            logging.error(f"Configuration file not found at {config_file_path}")
+            raise FileNotFoundError(f"Configuration file not found at {config_file_path}")
+        
+        config.read(config_file_path)
+        logging.info("Configuration file has been read successfully")
 
-    # Retrieve paths from the config and validate each
-    pdf_form = config['paths']['PDF_FORM']
-    validate_file_path(pdf_form)
-    print(pdf_form)
+        # Retrieve paths from the config and validate each
+        pdf_form = config['paths']['PDF_FORM']
+        validate_file_path(pdf_form)
+        print(pdf_form)
 
-    word_form = config['paths']['WORD_FORM']
-    validate_file_path(word_form)
-    print(word_form)
+        word_form = config['paths']['WORD_FORM']
+        validate_file_path(word_form)
+        print(word_form)
 
-    input_excel = config['paths']['INPUT_EXCEL']
-    validate_file_path(input_excel)
-    print(input_excel)
+        input_excel = config['paths']['INPUT_EXCEL']
+        validate_file_path(input_excel)
+        print(input_excel)
 
-    input_db = config['paths']['INPUT_DB']
-    validate_file_path(input_db)
-    print(input_db)
+        input_db = config['paths']['INPUT_DB']
+        validate_file_path(input_db)
+        print(input_db)
 
-    output = config['paths']['OUTPUT']
-    validate_file_path(output)
-    print(output)
+        output = config['paths']['OUTPUT']
+        validate_file_path(output)
+        print(output)
 
-except KeyError as e:
-    logging.error(f"Missing section or key in configuration file: {e}")
-    raise
-except configparser.Error as e:
-    logging.error(f"Error parsing the configuration file: {e}")
-    raise
+    except KeyError as e:
+        logging.error(f"Missing section or key in configuration file: {e}")
+        raise
+    except configparser.Error as e:
+        logging.error(f"Error parsing the configuration file: {e}")
+        raise
